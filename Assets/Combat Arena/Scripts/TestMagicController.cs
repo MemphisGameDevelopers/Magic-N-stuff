7f67ae54f856f88565bbba1fee92061af51f6e6a
Devitt Upkins <evilotaku@gmail.com>
2013-11-18 20:36:40 -0600

    Added Combat animation controller to WizardGirl
    I am working with the Mecanim WizardGirl in the Combat Arena

---
+++ Assets/Combat Arena/Scripts/TestMagicController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMagicController : MonoBehaviour
+{
+    private Animator anim;
+	// Use this for initialization
+	void Start ()
+	{
+	    anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (Input.GetKeyDown("q"))
+	    {
+            anim.SetTrigger("Circle");
+	    }
+
+        if (Input.GetKeyDown("w"))
+        {
+            anim.SetTrigger("Forward");
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            anim.SetTrigger("Ground");
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            anim.SetTrigger("Upwards");
+        }
+  
+
+	}
+}
